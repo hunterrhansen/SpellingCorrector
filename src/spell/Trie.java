3a114638f92d23d604e8f51ea8d6e71b8c98b25d
Hunter Hansen <hunterhansen50@gmail.com>
2022-04-27 13:06:23 -0600

    add profile tabs

--- src/spell/Trie.java
+++ src/spell/Trie.java
@@ -20,4 +20,19 @@ public class Trie implements ITrie {
   public int getNodeCount() {
     return 0;
   }
+
+  @Override
+  public int hashCode() {
+    return super.hashCode();
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    return super.equals(obj);
+  }
+
+  @Override
+  public String toString() {
+    return super.toString();
+  }
 }
